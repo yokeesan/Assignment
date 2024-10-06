3467e3d3f5750cc1a49e1401b36ea5d593765ad9
yokeesan <87594425+yokeesan@users.noreply.github.com>
2024-10-06 16:40:08 +0530

    Add files via upload

---
+++ next-assignment/src/assets/assets.js
@@ -0,0 +1,25 @@
+import P1 from './P1.png'
+import P2 from './P2.png'
+import P3 from './P3.png'
+import P4 from './P4.png'
+import P5 from './P5.png'
+import P6 from './P6.png'
+import Next from './next.png'
+import Previous from './previous.png'
+import Clock from './clock.png'
+import User from './user.png'
+
+
+export const assets = {
+    P1,
+    P2,
+    P3,
+    P4,
+    P5,
+    P6,
+    Next,
+    Previous,
+    Clock,
+    User,
+
+}
